fa7dd4335d30abbc529fc2a8975ab421ecc7f93e
Alice Nilsson <alice.nilsson@vgregion.se>
2026-08-11 10:06:28 +0200

    Add workflow for normalizing using histogram matching

---
+++ src/spinemira/workflows/normalization/histogram/__init__.py
@@ -0,0 +1,9 @@
+from .workflow import (
+    multiple_regions_histogram_matching_single_image_workflow,
+    multiple_regions_histogram_matching,
+)
+
+__all__ = [
+    "multiple_regions_histogram_matching",
+    "multiple_regions_histogram_matching_single_image_workflow",
+]
